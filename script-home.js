06b6c1d27196d476c882871fc9242cf174e4ae36
Vanesa Aguay Guerra <143807652+vaguay@users.noreply.github.com>
2026-01-10 15:52:52 -0500

    cam

--- script-home.js
+++ script-home.js
@@ -193,7 +193,7 @@ function startScreenAnimation() {
       0.1,
       1000
     );
-    camera.position.set(0, 1.6, 14);
+    camera.position.set(1, 1.6, 14);
     camera.lookAt(0, 0, 0);
   
     // Renderer
